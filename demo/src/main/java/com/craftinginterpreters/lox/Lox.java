69540885b2d9d560834548e5efef9a5e88e805db
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-10-28 23:17:15 -0300

    Adição do método runPrompt

--- demo/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ demo/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -9,19 +9,31 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-  public static void main(String[] args) throws IOException {
-    if (args.length > 1) {
-      System.out.println("Usage: jlox [script]");
-      System.exit(64); 
-    } else if (args.length == 1) {
-      runFile(args[0]);
-    } else {
-      runPrompt();
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64); 
+    }   else if (args.length == 1) {
+            runFile(args[0]);
+    }   else {
+            runPrompt();
     }
   }
 
-  private static void runFile(String path) throws IOException {
-    byte[] bytes = Files.readAllBytes(Paths.get(path));
-    run(new String(bytes, Charset.defaultCharset()));
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+  }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) { 
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+    }
   }
 }
