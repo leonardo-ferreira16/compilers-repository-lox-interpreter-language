7701e1f35c13b62d7e4a53d366b06d93793f7741
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-11-24 22:44:49 -0300

    Adição do método visitBinaryExpr()

--- demo/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ demo/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -17,6 +17,23 @@ class Interpreter implements Expr.Visitor<Object> {
     }
 
     @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right); 
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+    }
+    // Unreachable.
+    return null;
+  }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
@@ -26,7 +43,6 @@ class Interpreter implements Expr.Visitor<Object> {
             case BANG:
                 return !isTruthy(right);
         }
-
         // Unreachable.
         return null;
     }
