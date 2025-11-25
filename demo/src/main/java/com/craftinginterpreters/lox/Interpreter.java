545518392674a46571982a188c3bd7f13940d7e1
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-11-24 22:39:42 -0300

    Adição do método visitGroupingExpr()

--- demo/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ demo/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -5,5 +5,11 @@ class Interpreter implements Expr.Visitor<Object> {
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
-  }
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
 }
