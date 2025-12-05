979a03503abb3ce9c401cfa9dc749bd9f8d70ce8
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-12-05 17:18:33 -0300

    Implementação de caso if na função statement

--- demo/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ demo/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -35,6 +35,7 @@ class Parser {
     }
 
     private Stmt statement() {
+        if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
