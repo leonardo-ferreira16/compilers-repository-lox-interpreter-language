e2d685dedf04e2afa6d84ddd02ac413182ca2e25
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2025-12-15 20:13:02 -0300

    Adição do método visitSetExpr

--- demo/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ demo/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -206,8 +206,10 @@ class Parser {
             if (expr instanceof Expr.Variable) {
                 Token name = ((Expr.Variable)expr).name;
                 return new Expr.Assign(name, value);
+            } else if (expr instanceof Expr.Get) {
+                Expr.Get get = (Expr.Get)expr;
+                return new Expr.Set(get.object, get.name, value);
             }
-
             throw error(equals, "Invalid assignment target.");
         }
 
